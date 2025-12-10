9f3231e7e04402f28be4ad84e092a72e6b59ee40
Emanuele <emanuel5014@hotmail.it>
2025-12-10 16:28:21 +0100

    Initial Commit

---
+++ IPTV5014/IPTV5014App.swift
@@ -0,0 +1,17 @@
+//
+//  IPTV5014App.swift
+//  IPTV5014
+//
+//  Created by emanuele on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IPTV5014App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
